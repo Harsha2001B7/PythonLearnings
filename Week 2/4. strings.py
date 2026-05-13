1c941779e7812419305695ec39329b069d665889
Harsha Vardhan <harshavardhanreddy159@gmail.com>
2026-05-13 23:26:19 +0530

    updated

--- Week 2/4. strings.py
+++ Week 2/4. strings.py
@@ -1,2 +1,13 @@
-for x in 'banana':
-    print(x)
+# for x in 'banana':
+#     print(x)
+
+
+# txt = "The best things in life are free!"
+# print("free" in txt)
+
+a = '   Hello, World'
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace('Hello','Hi'))
+print(a.split(','))
