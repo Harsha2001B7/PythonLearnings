371a4eea8341e41e0faff82526d675107e9bb9a0
Harsha Vardhan <harshavardhanreddy159@gmail.com>
2026-06-03 21:52:36 +0530

    updated

---
+++ Week 4/6. print-consecutive.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    n= int(input())
+
+i = 1
+for i in range(1,n+1):
+    print(i,end="")
+
+print()
+if __name__ == '__main__':
+    n = int(input())
+
+    i = 1
+
+    while i <= n:
+        print(i,end="")
+        i += 1
