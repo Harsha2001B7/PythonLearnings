a2e4890c1af208e194f67c1fd1f3686aa0bc01c8
Harsha Vardhan <harshavardhanreddy159@gmail.com>
2026-06-12 20:33:54 +0530

    modified

--- Ai/test_gemini.py
+++ Ai/test_gemini.py
@@ -1,7 +1,7 @@
 # test_gemini.py
 from google import genai
 
-client = genai.Client(api_key="AQ.Ab8RN6KCbu8xWAOANYyxEfQ2-uTs5iofyZvA7u_7JQOz4ee4Kg")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
