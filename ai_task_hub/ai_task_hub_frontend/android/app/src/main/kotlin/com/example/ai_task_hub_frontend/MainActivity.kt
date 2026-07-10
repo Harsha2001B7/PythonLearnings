8f04112e1de970b08ee26eb647e7f05be59864b2
Harsha Vardhan <harshavardhanreddy159@gmail.com>
2026-07-10 16:52:18 +0530

    Final Clean up

--- ai_task_hub/ai_task_hub_frontend/android/app/src/main/kotlin/com/example/ai_task_hub_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ai_task_hub_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- trailer_hub/android/app/src/main/kotlin/com/example/trailer_hub/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.trailer_hub
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
